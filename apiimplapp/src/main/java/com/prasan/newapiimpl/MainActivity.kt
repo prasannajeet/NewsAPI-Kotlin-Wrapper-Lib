1c3796c2fd617e0e2ff2fc56918d3f6d6c23ffaf
Prasan Pani <prasannajeet89@gmail.com>
2020-07-31 19:52:49 -0400

    Added app-side implementation for first API

--- apiimplapp/src/main/java/com/prasan/newapiimpl/MainActivity.kt
+++ apiimplapp/src/main/java/com/prasan/newapiimpl/MainActivity.kt
@@ -2,10 +2,28 @@ package com.prasan.newapiimpl
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.prasan.newsapi_lib.NewsAPIHandler
+import com.prasan.newsapi_lib.showToast
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch { //Bad idea to use GlobalScope, only shown for demonstration purpose
+            NewsAPIHandler.with(BuildConfig.API_KEY).getNewsSources(
+                {
+                    showToast(it.hashCode().toString())
+                },
+                {
+                    showToast(it.message!!)
+                }
+            )
+        }
     }
 }
